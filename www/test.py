fd2625c76214768ff326224cf2f7856fce30afcf
Gladysgong <531404376@qq.com>
2017-08-09 19:33:01 +0800

    orm and test

---
+++ www/test.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+import www.orm, asyncio
+from www.models import User, Blog, Comment
+
+async def test(loop):
+    await www.orm.create_pool(loop = loop, user = 'root', password = '421498', db = 'awesome')
+    u = User(name = 'me', email = 'test2@example.nb', passwd = '1234567890',image = 'about:blank')
+    await u.save()
+
+loop = asyncio.get_event_loop()
+loop.run_until_complete(test(loop))
+loop.close()
