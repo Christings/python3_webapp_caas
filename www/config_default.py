eeaea85966fc02190410df0a584b933b4f3330b5
Gladysgong <531404376@qq.com>
2017-08-10 10:44:25 +0800

    web and config

---
+++ www/config_default.py
@@ -0,0 +1,14 @@
+# config_default.py
+
+configs = {
+    'db': {
+        'host': 'localhost',
+        'port': 3306,
+        'user': 'root',
+        'password': '421498',
+        'database': 'awesome'
+    },
+    'session': {
+        'secret': 'AwEsOmE'
+    }
+}
