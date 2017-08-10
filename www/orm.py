25cafd1e00f1d771e13a2be4a4c509485805cb4d
Gladysgong <531404376@qq.com>
2017-08-10 14:17:40 +0800

    some tips

--- www/orm.py
+++ www/orm.py
@@ -3,7 +3,7 @@
 __author__ = 'Lily Gong'
 
 import asyncio, logging
-
+# 一次使用异步，处处使用异步
 import aiomysql
 
 
@@ -11,9 +11,14 @@ def log(sql, args=()):
     logging.info('SQL: %s' % sql)
 
 
-async def create_pool(loop, **kw):
+async def create_pool(loop, **kw):  # 这里的kw是一个dict
     logging.info('create database connection pool...')
     global __pool
+    # 理解这里的yield from 是很重要的
+    # dict有一个get方法，如果dict中有对应的value值，则返回对应于key的value值，否则返回默认值，例如下面的host，如果dict里面没有
+    # 'host',则返回后面的默认值，也就是'localhost'
+    # 这里有一个关于Pool的连接，讲了一些Pool的知识点，挺不错的，<a target="_blank" href="http://aiomysql.readthedocs.io/en/latest/pool.html">点击打开链接</a>，下面这些参数都会讲到，以及destroy__pool里面的
+    # wait_closed()
     __pool = await aiomysql.create_pool(
         host=kw.get('host', 'localhost'),
         port=kw.get('port', 3306),
@@ -21,11 +26,18 @@ async def create_pool(loop, **kw):
         password=kw['password'],
         db=kw['db'],
         charset=kw.get('charset', 'utf8'),
-        autocommit=kw.get('autocommit', True),
+        autocommit=kw.get('autocommit', True),  # 默认自动提交事务，不用手动去提交事务。
         maxsize=kw.get('maxsize', 10),
         minsize=kw.get('minsize', 1),
         loop=loop
     )
+
+
+# async def destroy_pool():
+#     global  __pool
+#     if __pool is not None:
+#         __pool.close()
+#         await __pool.wait_closed()
 
 
 async def select(sql, args, size=None):
@@ -42,14 +54,21 @@ async def select(sql, args, size=None):
         return rs
 
 
+# 封装INSERT, UPDATE, DELETE
+# 语句操作参数一样，所以定义一个通用的执行函数，只是操作参数一样，但是语句的格式不一样
+# 返回操作影响的行号
+# 我想说的是 知道影响行号有个叼用
+
 async def execute(sql, args, autocommit=True):
     log(sql)
     async with __pool.get() as conn:
         if not autocommit:
             await conn.begin()
         try:
+            # 因为execute类型sql操作返回结果只有行号，不需要dict
             async with conn.cursor(aiomysql.DictCursor) as cur:
-                await cur.execute(sql.replace('?', '%s'), args)
+                # 顺便说一下 后面的args 别掉了 掉了是无论如何都插入不了数据的
+                await cur.execute(sql.replace('?', '%s'), args)  # 这里为什么还要手动提交数据
                 affected = cur.rowcount
             if not autocommit:
                 await conn.commit()
@@ -62,7 +81,8 @@ async def execute(sql, args, autocommit=True):
         return affected
 
 
-
+# 这个函数主要是把查询字段计数 替换成sql识别的?
+# 比如说：insert into  `User` (`password`, `email`, `name`, `id`) values (?,?,?,?)  看到了么 后面这四个问号
 def create_args_string(num):
     L = []
     for n in range(num):
@@ -70,26 +90,35 @@ def create_args_string(num):
     return ', '.join(L)
 
 
+# 定义Field类，负责保存(数据库)表的字段名和字段类型
+
 class Field(object):
+    # 表的字段包含名字、类型、是否为表的主键和默认值
     def __init__(self, name, column_type, primary_key, default):
         self.name = name
         self.column_type = column_type
         self.primary_key = primary_key
         self.default = default
 
+    # 返回 表名字 字段名 和字段类型
     def __str__(self):
         return '<%s, %s:%s>' % (self.__class__.__name__, self.column_type, self.name)
 
+
+# 定义数据库中五个存储类型
 
 class StringField(Field):
     def __init__(self, name=None, primary_key=False, default=None, ddl='varchar(100)'):
         super().__init__(name, ddl, primary_key, default)
 
 
+# 布尔类型不可以作为主键
+
 class BooleanField(Field):
     def __init__(self, name=None, default=False):
         super().__init__(name, 'boolean', False, default)
 
+# 不知道这个column type是否可以自己定义 先自己定义看一下
 
 class IntegerField(Field):
     def __init__(self, name=None, primary_key=False, default=0):
@@ -136,8 +165,10 @@ class ModelMetaclass(type):
         attrs['__primary_key__'] = primaryKey  # 主键属性名
         attrs['__fields__'] = fields  # 除主键外的属性名
         attrs['__select__'] = 'select `%s`, %s from `%s`' % (primaryKey, ', '.join(escaped_fields), tableName)
-        attrs['__insert__'] = 'insert into `%s` (%s, `%s`) values (%s)' % (tableName, ', '.join(escaped_fields), primaryKey, create_args_string(len(escaped_fields) + 1))
-        attrs['__update__'] = 'update `%s` set %s where `%s`=?' % (tableName, ', '.join(map(lambda f: '`%s`=?' % (mappings.get(f).name or f), fields)), primaryKey)
+        attrs['__insert__'] = 'insert into `%s` (%s, `%s`) values (%s)' % (
+            tableName, ', '.join(escaped_fields), primaryKey, create_args_string(len(escaped_fields) + 1))
+        attrs['__update__'] = 'update `%s` set %s where `%s`=?' % (
+            tableName, ', '.join(map(lambda f: '`%s`=?' % (mappings.get(f).name or f), fields)), primaryKey)
         attrs['__delete__'] = 'delete from `%s` where `%s`=?' % (tableName, primaryKey)
         return type.__new__(cls, name, bases, attrs)
 
